94ca9b53822adaa09108641d7da2c5dfa77ffc52
yyls <2141778598@qq.com>
2025-05-17 21:44:51 +0800

    feat:创建项目

---
+++ src/main/java/org/example/bws/BwsApplication.java
@@ -0,0 +1,13 @@
+package org.example.bws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bws/BwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
